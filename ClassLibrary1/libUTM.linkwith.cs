057bfa8565109038abb292b0730d83cd7603b41a
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2020-04-22 15:56:59 -0700

    add new project

---
+++ ClassLibrary1/libUTM.linkwith.cs
@@ -0,0 +1,3 @@
+﻿using ObjCRuntime;
+
+[assembly: LinkWith("libcocos2diOS.a", LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true, Frameworks = "", LinkerFlags = "-lstdc++ -lz", SmartLink = true, IsCxx = true)]
